1206fef619105e363e29e81bd273146343f16066
Ahmed Wagih <ahmd.wagih96@gmail.com>
2023-04-09 15:17:29 +0200

    created the Auth UI

--- src/app/components/navbar/UserMenu.tsx
+++ src/app/components/navbar/UserMenu.tsx
@@ -3,12 +3,15 @@ import { AiOutlineMenu } from "react-icons/ai";
 import Avatar from "../Avatar";
 import { useCallback, useState } from "react";
 import MenuItem from "./MenuItem";
+import useRegisterModal from "@/app/hooks/useRegisterModal";
 
 function UserMenu() {
   const [isOpen, setIsOpen] = useState<boolean>(false);
   const toggleOpen = useCallback(() => {
     setIsOpen((value) => !value);
   }, []);
+  const registerModal = useRegisterModal();
+  // console.log(registerModal.isOpen);
   return (
     <div className="relative">
       <div className="flex flex-row items-center gap-3">
@@ -35,7 +38,7 @@ function UserMenu() {
           <div className="flex flex-col cursor-pointer">
             <>
               <MenuItem onClick={() => {}} label="Login" />
-              <MenuItem onClick={() => {}} label="Sign-up" />
+              <MenuItem onClick={registerModal.onOpen} label="Sign-up" />
             </>
           </div>
         </div>
